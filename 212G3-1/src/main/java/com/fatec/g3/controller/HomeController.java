b52404057e5d235c5f4aa42b0012c6daaa1794b0
Silvio Rodrigues Correia /RA: 1042852352 <silviorodriguescorreia@outlook.com>
2021-09-14 17:58:40 -0300

    Versão Inicial

---
+++ 212G3-1/src/main/java/com/fatec/g3/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.fatec.g3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public ModelAndView menu() {
+		return new ModelAndView("paginaMenu");
+	}
+
+	@GetMapping("/clientes")
+	public ModelAndView cadastrarCliente() {
+		return new ModelAndView("cadastrarCliente");
+	}
+
+	@GetMapping("/fornecedores")
+	public ModelAndView cadastrarFornecedor() {
+		return new ModelAndView("cadastrarFornecedor");
+	}
+
+}
